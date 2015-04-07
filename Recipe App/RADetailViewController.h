1ea1d9eeee1f8517bc237ff291b8f16daed1f149
Eric Newman <deatomizer87@yahoo.com>
2015-04-07 15:42:27 -0600

    Creates detail view controller

---
+++ Recipe App/RADetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  RADetailViewController.h
+//  Recipe App
+//
+//  Created by Eric Newman on 4/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RARecipes.h"
+
+@interface RADetailViewController : UIViewController
+
+@property (assign, nonatomic) NSInteger recipeIndex;
+
+
+@end
